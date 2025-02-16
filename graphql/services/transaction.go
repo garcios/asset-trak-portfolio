16ddd904d606c3ac48cb61bd165ed8c8d7f88461
garcios <cm1017pg@outlook.com>
2025-02-16 15:16:43 +1100

    fix transaction svc

--- graphql/services/transaction.go
+++ graphql/services/transaction.go
@@ -18,7 +18,7 @@ func NewTransactionService() ITransactionService {
 	return &TransactionService{}
 }
 
-func (t *TransactionService) GetHoldingsSummary(ctx context.Context,
+func (t TransactionService) GetHoldingsSummary(ctx context.Context,
 	accountID string,
 ) (*pb.BalanceSummaryResponse, error) {
 	resolverClient := micro.NewService(micro.Name("transaction-resolver.client"))
