a259c11b8a0f27373d8ea6bc08e7418ba2256f79
garcios <cm1017pg@outlook.com>
2025-02-16 12:26:12 +1100

    refactor query resolvers

--- graphql/resolvers/holdings.go
+++ graphql/resolvers/holdings.go
@@ -66,38 +66,6 @@ func (r *queryResolver) GetHoldingsSummary(ctx context.Context, accountID string
 	return investments, nil
 }
 
-func (r *queryResolver) GetSummaryTotals(ctx context.Context, accountID string) (*models.SummaryTotals, error) {
-	// TODO: implement this with call to transaction service
-	summaryTotals := &models.SummaryTotals{
-		PortfolioValue: &models.Money{
-			Amount:       250000,
-			CurrencyCode: "AUD",
-		},
-		CapitalGain: &models.MoneyWithPercentage{
-			Amount:       42000,
-			CurrencyCode: "AUD",
-			Percentage:   18,
-		},
-		Dividends: &models.MoneyWithPercentage{
-			Amount:       1200,
-			CurrencyCode: "AUD",
-			Percentage:   2.5,
-		},
-		CurrencyGain: &models.MoneyWithPercentage{
-			Amount:       259,
-			CurrencyCode: "AUD",
-			Percentage:   0.54,
-		},
-		TotalReturn: &models.MoneyWithPercentage{
-			Amount:       52000,
-			CurrencyCode: "AUD",
-			Percentage:   24,
-		},
-	}
-
-	return summaryTotals, nil
-}
-
 func toMoney(m *pb.Money) *models.Money {
 	return &models.Money{
 		Amount:       m.Amount,
