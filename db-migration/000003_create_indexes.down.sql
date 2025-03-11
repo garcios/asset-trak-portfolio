5fdda89ea1c4bbe8df09747107940b94b86a8125
garcios <cm1017pg@outlook.com>
2025-03-11 21:52:04 +1100

    cleanup db migrations

---
+++ db-migration/000003_create_indexes.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX idx_transaction_type_filtered ON transaction;
+
+DROP INDEX idx_transaction_asset_date_type ON transaction;
+
+DROP INDEX idx_asset_price_covering ON asset_price;
+
+DROP INDEX idx_asset_price_asset_date ON asset_price;
+
--- db-migration/000003_portfolio_value_mg.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS portfolio_performance;
-
-DROP TABLE IF EXISTS portfolio_value;
